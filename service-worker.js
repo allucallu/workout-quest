bbf70031ce7f465d763d85d5610cd32045c7acc1
allucallu <95993840+allucallu@users.noreply.github.com>
2025-02-20 15:08:07 +0800

    tambah manifest

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'workout-quest-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/home.html',
+  '/profile.html',
+  '/styles.css',
+  '/app.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch assets from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
